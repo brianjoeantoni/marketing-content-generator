e733483a05de73c022b948311f9082a54e0b2f6d
KODEFY <kodefy.web@gmail.com>
2026-09-02 15:42:29 +0700

    chore: add comments

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -5,11 +5,11 @@ import { authRouter } from "./routes/auth.js";
 import { postersRouter } from "./routes/posters.js";
 import cookieParser from "cookie-parser";
 
-const app = express();
+const app = express(); // creates the Express app
 const PORT = Number(process.env.PORT) || 4000;
 
 app.use(express.json()); // parses incoming requests with JSON payloads
-app.use(cookieParser()); // reads incoming cookies and makes them available later on:
+app.use(cookieParser()); // reads incoming cookies and makes them available later on
 
 // logger middleware
 app.use((req, _res, next) => {
@@ -33,6 +33,7 @@ app.get("/health", async (_req, res) => {
 app.use("/api/auth", authRouter);
 app.use("/api/posters", postersRouter);
 
+// starts the HTTP server
 app.listen(PORT, () => {
   console.log(`API server running on http://localhost:${PORT}`);
 });
